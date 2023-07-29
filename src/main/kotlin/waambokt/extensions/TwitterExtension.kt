37afc56749eb2107676d0119d82b098e3d5f2d6a
Andrew Miller <andrewm192@outlook.com>
2023-07-28 23:43:24 -0400

    fix embed construction and add icon

--- src/main/kotlin/waambokt/extensions/TwitterExtension.kt
+++ src/main/kotlin/waambokt/extensions/TwitterExtension.kt
@@ -2,6 +2,7 @@ package waambokt.extensions
 
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.event
+import dev.kord.common.Color
 import dev.kord.core.behavior.reply
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.rest.builder.message.create.embed
@@ -27,7 +28,16 @@ class TwitterExtension : Extension() {
                     extractIt<ScrapingResult> { htmlDocument { findFirst(notesClass) }.children.getContent(it) }
                 }
 
-                event.message.reply { embed { title = notesHead; description = result.texts.joinToString { "\n" } } }
+                event.message.reply {
+                    embed {
+                        author {
+                            name = "Community Notes:"
+                            icon = "https://abs.twimg.com/icons/apple-touch-icon-192x192.png"
+                        }
+                        description = result.texts.joinToString("\n")
+                        color = Color(embedColor)
+                    }
+                }
             }
         }
     }
@@ -42,8 +52,8 @@ class TwitterExtension : Extension() {
         private val linkRegex = """https?://(?:www\.)?twitter\.com/([a-zA-Z0-9_]+)/status/([0-9]+)""".toRegex()
         private const val notesClass = ".css-901oao.r-37j5jr.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-1e081e0.r-qvutc0"
         private const val agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
-        private const val notesHead = "Readers added context they thought people might want to know"
         private const val timeoutMs = 60_000
+        private const val embedColor = 0x1DA0F2
     }
 
     data class ScrapingResult(val texts: MutableList<String> = mutableListOf())
